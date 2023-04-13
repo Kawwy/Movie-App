a3b790206fbe5c31df3b9415d055a084b41f62c7
Kawin <100144750+Kawwy@users.noreply.github.com>
2023-04-13 22:31:43 +0530

    Update App.js
    Fixing https issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 
 // 64f4faa2
 
-const API_URL = "http://www.omdbapi.com?apikey=64f4faa2";
+const API_URL = "https://www.omdbapi.com?apikey=64f4faa2";
 
 const App = () => {
     const [movies, setMovies] = useState([]);
